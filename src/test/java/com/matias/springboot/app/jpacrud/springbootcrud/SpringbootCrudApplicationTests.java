afdf3b1f131977dbd9371a52d97d33899c3da06e
Matias Navio <naviomatias2@gmail.com>
2024-04-05 18:38:15 -0300

    Initial commit

---
+++ src/test/java/com/matias/springboot/app/jpacrud/springbootcrud/SpringbootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matias.springboot.app.jpacrud.springbootcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
